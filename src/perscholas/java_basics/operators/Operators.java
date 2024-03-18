894d3087b47b924f73ba45868633cb7f55292812
johnpaulfernandez <johnpaulvfernandez@gmail.com>
2024-03-18 09:52:53 -0700

    GLAB 303.2.1 - Core Java: Operators

---
+++ src/perscholas/java_basics/operators/Operators.java
@@ -0,0 +1,41 @@
+package perscholas.java_basics.operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        int x, y = 10, z = 5;
+        x = y + z;
+        System.out.println("+ operator resulted in " + x);
+        x = y - z;
+        System.out.println("- operator resulted in " + x);
+        x = y * z;
+        System.out.println("* operator resulted in " + x);
+        x = y / z;
+        System.out.println("/ operator resulted in " + x);
+        x = y % z;
+        System.out.println("% operator resulted in " + x);
+        x = y++;
+        System.out.println("Postfix ++ operator resulted in " + x);
+        x = ++z;
+        System.out.println("Prefix ++ operator resulted in " + x);
+        x = -y;
+        System.out.println("Unary operator resulted in " + x);
+        // Some examples of special Cases
+        // Integer.MAX_VALUE: 2147483647
+        int tooBig = Integer.MAX_VALUE + 1; // -2147483648
+        // Integer.MIN_VALUE: -2147483648
+        int tooSmall = Integer.MIN_VALUE - 1; // 2147483647
+        // Integer.MAX_VALUE.
+        System.out.println("Integer.MAX_VALUE: " + Integer.MAX_VALUE);
+        System.out.println("Integer.MIN_VALUE: " + Integer.MIN_VALUE);
+        System.out.println("tooBig becomes " + tooBig);
+        System.out.println("tooSmall becomes " + tooSmall);
+        System.out.println(4.0 / 0.0); // Prints: Infinity
+        System.out.println(-4.0 / 0.0); // Prints: -Infinity
+        System.out.println(0.0 / 0.0); // Prints: NaN
+        double d1 = 12 / 8; // result: 1 by integer division. The value of d1 becomes 1.0.
+        double d2 = 12.0F / 8; // result: 1.5
+        System.out.println("d1 is " + d1);
+        System.out.println("d2 is " + d2);
+
+    }
+}
